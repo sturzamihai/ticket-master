8b65e5bf38d00d700ffbac46976469a9e962b1bf
Mihai Sturza <mihai.sturza@nevexo.ro>
2023-01-19 01:07:33 +0200

    Deleted unused files

--- PDFMicroservice/dist/app.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = require('./routes/index.ts');
-const app = express();
-app.use(router);
-app.listen(8080, () => { console.log("TicketMaster PDF Microservice running on 8080."); });
-module.exports = app;
--- PDFMicroservice/dist/services/pdf-microservice.js
+++
@@ -1,9 +0,0 @@
-const PDFDocument = require("pdfkit");
-function generareBilet(dateBilet, dataCallback, endCallback) {
-    const bilet = new PDFDocument({ bufferPages: 'true', font: 'Courier' });
-    bilet.on('data', dataCallback);
-    bilet.on('end', endCallback);
-    bilet.fontSize(20).text(dateBilet.numeEveniment);
-    bilet.end();
-}
-module.exports = generareBilet;
